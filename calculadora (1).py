0870ad5503f916ee44613a613cf1f8e4a3ce39c8
suicidestoleart <evelynguimaraes1905@gmail.com>
2021-08-18 17:45:57 -0300

    Adicionando arqivos
    Adicionando os arquivos em python feitos em aula e também em casa ou no caso da Batata Canibal, apartamento, na ctrl+play

---
+++ calculadora (1).py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import math
+def soma(a, b):
+    return a+b
+def subtracao(a, b):
+    return a-b
+def multiplicacao(a, b):
+    return a*b
+def divisao(a, b):
+    return a/b
+def exponenciacao(a, b):
+    return math.pow(a, b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
